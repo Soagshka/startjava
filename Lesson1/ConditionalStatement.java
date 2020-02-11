c39d628478928455c4bb8ffdf4f733f0fcfe4213
Alexander Gorbenko <Alexander.Gorbenko@sas.com>
2020-02-11 17:44:43 +0300

    Lesson 1 v0.1

---
+++ Lesson1/ConditionalStatement.java
@@ -0,0 +1,48 @@
+public class ConditionalStatement
+{
+	public static void main(String[] args)
+	{
+		short age = 39;
+		char sex = 'M';
+		double height = 1.92;
+		char firstNameLetter = 'I';
+
+		System.out.println("age === " + age);
+		System.out.println("sex === " + sex);
+		System.out.println("height === " + height);
+		System.out.println("firstNameLetter === " + firstNameLetter);
+
+		if (age>20)
+		{
+			System.out.println("Age more then 20!");
+		}
+		if (sex=='M')
+		{
+			System.out.println("Male");
+		}
+		if (sex!='M')
+		{
+			System.out.println("Female");
+		}
+		if (height>1.80)
+		{
+			System.out.println("height more then 1.80");
+		}
+		else
+		{
+			System.out.println("height less then 1.80");
+		}
+		if (firstNameLetter == 'M')
+		{
+			System.out.println("First Name starts with letter M");
+		}
+		else if (firstNameLetter == 'I')
+		{
+			System.out.println("First Name starts with letter I");
+		}
+		else
+		{
+			System.out.println("First Name starts with some other letter");
+		}	
+	}
+}
---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,28 @@
+public class MyFirstGame
+{
+	public static void main(String[] args)
+	{
+
+		int value = 45;
+		int i = 0;
+		boolean isGuessed = false;
+
+		while (isGuessed==false)
+		{
+			if (i<value)
+			{
+				System.out.println("Введенное вами число меньше того, что загадал компьютер");
+			}
+			else if (i>value)
+			{
+				System.out.println("Введенное вами число больше того, что загадал компьютер");
+			}
+			else
+			{
+				System.out.println("Вы угадали!");
+				isGuessed = true;
+			}
+		}
+		
+	}
+}
