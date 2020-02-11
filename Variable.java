2cf44360be5ac697893c501537e4a123848c6f90
Alexander Gorbenko <Alexander.Gorbenko@sas.com>
2020-02-11 15:12:47 +0300

    added Variable.java

---
+++ Variable.java
@@ -0,0 +1,23 @@
+public class Variable
+{
+	public static void main(String[] args)
+	{
+		byte byteValue = -8;
+		short bit = 64;
+		int processorTypeInt = 8650;
+		long processorTypeLong = 8650;
+		float cpuFrequency = 2.11f;
+		double installedMemory = 31.9;
+		char charValue = 'S';
+		boolean booleanValue = true;
+
+		System.out.println(" byteValue === " + byteValue);
+		System.out.println(" bit === " + bit);
+		System.out.println(" processorTypeInt === " + processorTypeInt);
+		System.out.println(" processorTypeLong === " + processorTypeLong);
+		System.out.println(" cpuFrequency === " + cpuFrequency);
+		System.out.println(" installedMemory === " + installedMemory);
+		System.out.println(" charValue === " + charValue);
+		System.out.println(" booleanValue === " + booleanValue);
+	}
+}
